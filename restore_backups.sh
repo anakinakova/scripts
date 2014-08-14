d1c9d30a6a17f9c8361feb0edf6c7ded723f6975
djordjana <ana@oomphhq.com>
2014-08-14 16:00:40 +1000

    I'm sick of manually downloading and restoring databases.

---
+++ restore_backups.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SACHIN='publish.oomphhq.com'
+DASHBOARD='ec2-54-82-222-73.compute-1.amazonaws.com'
+ADOMATIC='ec2-174-129-102-12.compute-1.amazonaws.com'
+
+function db_restore
+{
+  echo $1
+  echo $2
+  DB_HOST=$2
+  APP_NAME=$1
+  ssh deploy@$DB_HOST '<<-_eof_
+    i="9"
+    while [ $i -ge 0 ]; do
+      sudo -i eybackup -e mysql --download $i:$APP_NAME
+      if [ $? -eq 0 ]; then
+        break;
+      fi
+      i=$[$i-1]
+    done
+  _eof_
+  '
+  BACKUP=`ssh deploy@$DB_HOST 'ls /mnt/tmp'`
+  scp deploy@$DB_HOST:/mnt/tmp/$BACKUP ~/db
+  mysql -u root $APP_NAME'_development' < ~/db/$BACKUP
+}
+
+db_restore sachin $SACHIN
+db_restore oompfserver $DASHBOARD
+db_restore adomatic $ADOMATIC
